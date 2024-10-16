5bdf44e4c8c3cdec2a9c596d167fdea0d76bc4d5
marvi2011 <marvi661@hotmail.com>
2024-10-16 13:25:43 -0300

    estructura del navbar lista... falta hacer detalles

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import NavMenu from './Components/Common/common/NavMenu';
 
 function App() {
-  return <></>;
+  return <>
+  <NavMenu></NavMenu>
+  <h1 className='text-center'>HOTEL REFUGIO DE LAS NUBES</h1>
+  </>;
 }
 
 export default App;
